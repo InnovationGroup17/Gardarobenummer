9f120098a34102e0c1b8b1165e15c808b0109b88
KasperVejen <kasper.ellegaard.vejen@gmail.com>
2023-09-18 21:29:32 +0200

    QR Scanner virker

--- gardarobeApp/components/stackComponents/QrScanner.js
+++ gardarobeApp/components/stackComponents/QrScanner.js
@@ -1,12 +1,54 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+// Date: 18.09.2020
+import { Button, StyleSheet, Text, View, Linking } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import * as React from "react";
 
 const QrScanner = () => {
-  return (
-    <View>
-      <Text>QrScanner</Text>
-    </View>
-  )
-}
+  const [hasPermission, setHasPermission] = React.useState(null);
+  const [scanned, setScanned] = React.useState(false);
 
-export default QrScanner
+  React.useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    Linking.openURL(data);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <View style={(styles.constainer, StyleSheet.absoluteFillObject)}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && (
+        <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
+      )}
+    </View>
+  );
+};
+
+export default QrScanner;
+
+const styles = StyleSheet.create({
+  constainer: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  button: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
