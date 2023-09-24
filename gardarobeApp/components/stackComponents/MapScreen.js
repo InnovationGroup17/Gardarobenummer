b6c68c773bf19840845dd2521b012a1f2b844067
Stau2000 <Emil2@live.dk>
2023-09-24 19:03:20 +0200

    Barer hentes fra database og vises på kortet

--- gardarobeApp/components/stackComponents/MapScreen.js
+++ gardarobeApp/components/stackComponents/MapScreen.js
@@ -1,31 +1,37 @@
 import React, { useState, useEffect } from "react";
-import MapView, { Marker} from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import { StyleSheet, View, Alert } from "react-native";
 import * as Location from "expo-location";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
-// Chateau Motel coordinates: 55.678153413526765, 12.574362797657678
-
-let locationOfInterest = [
-  {
-    title: "Chateau Motel",
-    location: { latitude: 55.678153413526765, longitude: 12.574362797657678 },
-    description: "Du har valgt Chateau Motel",
-  },
-];
+// Initialize Firebase with your Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCb-as5hyWyAqOqZP1_-1ZAYjX0pXx2tBg",
+  authDomain: "database-95c46.firebaseapp.com",
+  projectId: "database-95c46",
+  databaseURL:
+    "https://database-95c46-default-rtdb.europe-west1.firebasedatabase.app/",
+  storageBucket: "database-95c46.appspot.com",
+  messagingSenderId: "693275199236",
+  appId: "1:693275199236:web:04332aed156a57e80bb251",
+};
+const firebaseApp = initializeApp(firebaseConfig);
+const firestore = getFirestore(firebaseApp);
 
 export default function MapScreen() {
   const [initialRegion, setInitialRegion] = useState(null);
+  const [locations, setLocations] = useState([]);
+  const [locationOfInterest, setLocationOfInterest] = useState([]); // Store the locations of interest
 
   useEffect(() => {
     (async () => {
-      // Request location permissions
       const { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
         Alert.alert("Permission to access location was denied");
         return;
       }
 
-      // Get the device's current location
       const location = await Location.getCurrentPositionAsync({});
       setInitialRegion({
         latitude: location.coords.latitude,
@@ -34,6 +40,46 @@ export default function MapScreen() {
         longitudeDelta: 0.0421,
       });
     })();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const locationsCollection = collection(firestore, "Bars");
+        const querySnapshot = await getDocs(locationsCollection);
+
+        const locationsData = [];
+        querySnapshot.forEach((doc) => {
+          const location = doc.data();
+          locationsData.push(location);
+        });
+
+        setLocations(locationsData);
+
+        // Log the fetched data
+        console.log("Fetched Data:", locationsData);
+
+        // Create objects for locationOfInterest using a for loop
+        const interestLocations = [];
+        for (let i = 0; i < locationsData.length; i++) {
+          interestLocations.push({
+            title: locationsData[i].title,
+            location: {
+              latitude: locationsData[i].location.latitude,
+              longitude: locationsData[i].location.longitude,
+            },
+            description: locationsData[i].description,
+          });
+        }
+
+        // Set the locationOfInterest state
+        setLocationOfInterest(interestLocations);
+      } catch (error) {
+        console.error("Error fetching data from Firestore:", error);
+      }
+    };
+
+    fetchData();
   }, []);
 
   const showLocationOfInterest = () => {
@@ -57,7 +103,6 @@ export default function MapScreen() {
           initialRegion={initialRegion}
           showsUserLocation={true}
         >
-          {/* Wrap the string within a <Text> component */}
           {showLocationOfInterest()}
         </MapView>
       ) : null}
--- gardarobeApp/firebaseConfig.js
+++ gardarobeApp/firebaseConfig.js
@@ -1,10 +1,12 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCb-as5hyWyAqOqZP1_-1ZAYjX0pXx2tBg",
-    authDomain: "database-95c46.firebaseapp.com",
-    projectId: "database-95c46",
-    storageBucket: "database-95c46.appspot.com",
-    messagingSenderId: "693275199236",
-    appId: "1:693275199236:web:04332aed156a57e80bb251",
-  };
-   { firebaseConfig };
+  apiKey: "AIzaSyCb-as5hyWyAqOqZP1_-1ZAYjX0pXx2tBg",
+  authDomain: "database-95c46.firebaseapp.com",
+  projectId: "database-95c46",
+  databaseURL:
+    "https://database-95c46-default-rtdb.europe-west1.firebasedatabase.app/",
+  storageBucket: "database-95c46.appspot.com",
+  messagingSenderId: "693275199236",
+  appId: "1:693275199236:web:04332aed156a57e80bb251",
+};
+ { firebaseConfig };
   
