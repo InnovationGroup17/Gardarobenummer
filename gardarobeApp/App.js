8f93db74f03bc67247c1325ea60de6aa968abe01
KasperVejen <kasper.ellegaard.vejen@gmail.com>
2023-10-09 15:13:22 +0200

    forfra med stripe frontend

--- gardarobeApp/App.js
+++ gardarobeApp/App.js
@@ -1,15 +1,8 @@
 import { StyleSheet } from "react-native";
-import React, { useEffect } from "react";
 import DrawerNavigator from "./components/navigation/DrawNavigator";
-import { initStripe } from "@stripe/stripe-react-native";
+
 
 export default function App() {
-  useEffect(() => {
-    initStripe({
-      publishableKey:
-        "pk_test_51NzHQxAu5ko6Dm7GK9HSzRghvBoupGKUR6lHq6HYKGsXwvivhQI2tjHgQdJmf9tOmfmqU8eUx18KPnTJzfHwJYEw00IesVKr6x",
-    });
-  }, []);
   return <DrawerNavigator />;
 }
 
